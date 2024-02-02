a5c3b5d086ce66dfa86dd005adc690e4f53757c8
Yuki <xchendaito@gmail.com>
2024-02-02 17:25:44 +0900

    sort

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -10,6 +10,26 @@
     <p>
         <?php
         // 
+        function sort_2way($array,$order){
+            $array=[15,4,18,23,10];
+            if($order){
+                echo"昇順で表示します<br>";
+                sort(array:$array);
+                foreach($array as $array){
+                echo $array.'<br>';
+            }
+        }
+        else{
+                echo"降順で表示します<br>";
+                rsort(array:$array);
+                foreach($array as $array){
+                echo $array.'<br>';
+            }
+        }}
+        $array = [15,4,18,23,10];
+        sort_2way($array, true);
+        sort_2way($array, false);
+
         ?>
     </p>
 </body>
